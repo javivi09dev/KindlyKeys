83426b43c0289357d616d7fc5e7c2c33783384d0
javivi09dev <javierotoalonso@gmail.com>
2024-11-16 15:31:34 +0100

    Error del Mixin de MixinPlayerEntity

--- src/main/java/me/javivi/kindlykeys/shared/mixin/MixinPlayerEntity.java
+++ src/main/java/me/javivi/kindlykeys/shared/mixin/MixinPlayerEntity.java
@@ -1,13 +1,13 @@
 package me.javivi.kindlykeys.shared.mixin;
 
 import me.javivi.kindlykeys.shared.objects.IServerSideChecker;
-import me.javivi.kindlykeys.shared.objects.Reference;
+import net.minecraft.network.syncher.EntityDataAccessor;
+import net.minecraft.network.syncher.EntityDataSerializers;
+import net.minecraft.network.syncher.SynchedEntityData;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
-import net.minecraft.network.syncher.EntityDataAccessor;
-import net.minecraft.network.syncher.SynchedEntityData;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -15,7 +15,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Player.class)
 public abstract class MixinPlayerEntity extends LivingEntity implements IServerSideChecker {
-    private static final EntityDataAccessor<Boolean> SERVERSIDE_PRESENT = SynchedEntityData.defineId(Player.class, Reference.SERVERSIDE_PRESENT);
+    private static final EntityDataAccessor<Boolean> SERVERSIDE_PRESENT = SynchedEntityData.defineId(Player.class, EntityDataSerializers.BOOLEAN);
 
     protected MixinPlayerEntity(EntityType<? extends LivingEntity> entityType, Level level) {
         super(entityType, level);
